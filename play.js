35b79028d77f3ea2696440b69c66f69ab9eff1d2
Abdullah Ahmad Zabir <zabir.aa@gmail.com>
2021-08-25 12:38:44 -0400

    Seperated the input module

--- play.js
+++ play.js
@@ -1,18 +1,5 @@
-const {connect} = require('./client');
-
-const handleUserInput = function(data) {
-  if (data === '\u0003') {
-    process.exit();
-  }
-};
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  stdin.on('data',handleUserInput);
-  return stdin;
-};
+const { connect } = require('./client');
+const { setupInput } = require('./input');
 
 // establishes a connection with the game server
 console.log("Connecting ...");
