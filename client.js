d89152a9850f714b324838f043fe597466d0afcb
Abdullah Ahmad Zabir <zabir.aa@gmail.com>
2021-08-24 21:20:24 -0400

    Added the sending of player name to the server

--- client.js
+++ client.js
@@ -8,6 +8,7 @@ const connect = function() {
   conn.setEncoding("utf8");
   conn.on('connect', () => {
     console.log("Successfully connected to the server");
+    conn.write('Name: AAZ');
   });
   conn.on("data", (data) => {
     console.log(data);
