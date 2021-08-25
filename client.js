23f67e4154ed7d18e8177265fc9105abe541286c
Abdullah Ahmad Zabir <zabir.aa@gmail.com>
2021-08-24 21:39:43 -0400

    tested the move commands

--- client.js
+++ client.js
@@ -10,6 +10,13 @@ const connect = function() {
     console.log("Successfully connected to the server");
     conn.write('Name: AAZ');
   });
+ /* conn.on('connect', () => {
+    conn.write('Move: up');
+    setInterval(() => conn.write('Move: left'),1000);
+    setInterval(() => conn.write('Move: down'),3000);
+    setInterval(() => conn.write('Move: right'),6000);
+    setInterval(() => conn.write('Move: up'),9000);
+  }); */
   conn.on("data", (data) => {
     console.log(data);
   });
