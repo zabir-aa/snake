c40194a958c3eaa6faebc02840b20f0bd77a5849
Abdullah Ahmad Zabir <zabir.aa@gmail.com>
2021-08-29 15:05:45 -0400

    run linter

--- README.md
+++ README.md
@@ -16,6 +16,8 @@ Before you can run this client, you will need to be running the server side whic
 
 - Follow steps inside the snek server repo to run the server side
 - Run the development snake client using the `node play.js` command.
+- Quicktexts: Press 1 to send "gotcha", 2 to sent "I win", and 3 to send "see ya".
+- Press tiled "`" key to chat if the extra chat feature is enabled.
 
 ## Extra Features
 An optional chat feature is available, although not part of the primary package. The required code and instructions can be found in the extra.js file. 
--- extra.js
+++ extra.js
@@ -1,9 +1,9 @@
 // For an experimental chatting feature - makes live chat possibe during gameplay
-// TThis feature contains a loop bug: 
+// TThis feature contains a loop bug:
 // After each message is sent, the connection and messages get one extra instance due to the extra call of the whole setupInput function.
 // Need to find an alternative to that in order to get back to gameplay from chat.
 // Steps:
-// 1. Put at the top of client.js file -> const readline = require('readline'); 
+// 1. Put at the top of client.js file -> const readline = require('readline');
 // 2. Put the below code inside the handleUserInput function after the last if condition ->
 
 /*if (data === '\`') {
